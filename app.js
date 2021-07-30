a528c2ac7b195c96167d7bb7401689fe630c2af8
Syed Ahamed <SYEDAHAMED29>
2021-07-31 03:37:09 +0530

    Removed md5 and added Bcrypt - Level 4

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const md5 = require('md5');
-
+const bcrypt = require('bcrypt');
+const salt = 10;
 
 const app = express();
 
@@ -38,21 +38,25 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
-app.post("/register", (req,res) =>{
+app.post("/register", (req, res) => {
 
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
+  bcrypt.hash(req.body.password, salt, function(err, hash) {
+
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+
   });
 
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }
-    else {
-      res.render("secrets");
-    }
-  });
 
 });
 
@@ -60,21 +64,26 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.post("/login", (req,res) =>{
+app.post("/login", (req, res) => {
 
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
 
-  User.findOne({email: username}, function(err, foundUser){
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
+
     if (foundUser) {
-      if(foundUser.password == password){
-        res.render("secrets");
-      }
-    }
-    else{
+      bcrypt.compare(password, foundUser.password, function(err, result) {
+
+        if (result == true) {
+          res.render("secrets");
+        }
+      });
+
+    } else {
       console.log(err);
-      
     }
   });
 });
